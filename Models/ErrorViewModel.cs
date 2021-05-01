9cb982b7122e0653b9727450a939e27694a2f3ab
Elías Emiliano Beltrán González <eliasemilianobg@gmail.com>
2021-05-01 14:29:12 -0500

    Iniciando proyecto mvc.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_de_Registro_de_Estudiantes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
