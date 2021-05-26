0fbdcc9d1252d5b3bd01b623392923d6a07950ac
Elías Emiliano Beltrán González <eliasemilianobg@gmail.com>
2021-05-25 23:54:00 -0500

    Cambiando cadena de conexion.

--- Models/RegistroEstudiantesContext.cs
+++ Models/RegistroEstudiantesContext.cs
@@ -28,7 +28,7 @@ namespace Sistema_de_Registro_de_Estudiantes.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=localhost;database=SRE_BD;user=SA;pwd=BDserviciosocialpass123");
+                optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;Initial Catalog=SRE_BD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
 
